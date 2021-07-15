e193f98b2a84607a775b556f26e27bc2b42ab796
Bea Stollnitz <bea@stollnitz.com>
2021-07-14 21:50:11 -0700

    Use training=false in predict function.

--- fashion-tf/src/main.py
+++ fashion-tf/src/main.py
@@ -152,7 +152,7 @@ def training_phase():
 
 @tf.function
 def predict(model: tf.keras.Model, X: np.ndarray) -> tf.Tensor:
-  y_prime = model(X)
+  y_prime = model(X, training=False)
   probabilities = tf.nn.softmax(y_prime, axis=1)
   predicted_indices = tf.math.argmax(input=probabilities, axis=1)
   return predicted_indices
