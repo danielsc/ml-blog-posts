359bba410087cba675a93018ac143746e7b19b6f
Bea Stollnitz <bea@stollnitz.com>
2021-07-21 11:49:20 -0700

    Changed sample request to contain a single image.

--- managed-endpoint/pytorch_src/create-sample-request.py
+++ managed-endpoint/pytorch_src/create-sample-request.py
@@ -1,34 +1,20 @@
-from torchvision import datasets
-import os
+import json
 
-# def create_sample_request() -> None:
-#   batch_size = 64
-#   (_, test_dataloader) = get_data(batch_size)
+from train import get_data
+
+
+def create_sample_request() -> None:
+  batch_size = 64
+  (_, test_dataloader) = get_data(batch_size)
   
-#   (X_batch, _) = next(iter(test_dataloader))
-#   X = X_batch[0:3, :, :, :].cpu().numpy().tolist()
-#   with open('sample_request/sample_request.json', 'w') as file:
-#     json.dump({ 'data': X }, file)
+  (X_batch, _) = next(iter(test_dataloader))
+  X = X_batch[0, 0, :, :].cpu().numpy().tolist()
+  with open('sample_request/sample_request.json', 'w') as file:
+    json.dump({ 'data': X }, file)
 
-
-# def main() -> None:
-#   create_sample_request()
 
 def main() -> None:
-  test_data = datasets.FashionMNIST(
-    root='data',
-    train=False,
-    download=True,
-  )
-
-  try:
-    os.mkdir('sample_request')
-  except:
-    pass
-  for i, (image, _) in enumerate(test_data):
-    if i == 1000:
-      break
-    image.save(f'sample_request/{i+1:0>4}.png')
+  create_sample_request()
 
 
 if __name__ == '__main__':
